4c3fa6d8b2af18944df2d95a000544ba11a9da51
webfaisalbd <web.faisal.bd@gmail.com>
2022-06-19 16:46:12 +0600

    function and arrow function

---
+++ day-5/app.js
@@ -0,0 +1,17 @@
+// normal function 
+function add (a,b){
+    return a+b;
+}
+console.log(add(20,10));
+
+
+// multi line arrow function
+const add2 = (a,b) => {
+    return a+b;
+}
+console.log(add2(20,10));
+
+// single line arrow function 
+const add3 = x => x+x;
+console.log(add3(20));
+
